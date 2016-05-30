f23370ec47c0940eb8d8b2714d6dadf1996bf613
wangxujie <wangxj0714@163.com>
2016-05-30 22:37:10 +0800

    Initial commit

---
+++ app/src/main/java/zz/itcast/androidnewfeature/AppBarLayoutActivity.java
@@ -0,0 +1,16 @@
+package zz.itcast.androidnewfeature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class AppBarLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_bar_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
